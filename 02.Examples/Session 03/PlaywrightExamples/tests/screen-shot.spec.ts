c30ee7c25e166f44b3865e3c54eb231d6372c002
Ngo Thanh Tung <tungnt@softech.vn>
2023-06-09 20:31:45 +0700

    Update

--- 02.Examples/Session 03/PlaywrightExamples/tests/screen-shot.spec.ts
+++ 02.Examples/Session 03/PlaywrightExamples/tests/screen-shot.spec.ts
@@ -18,11 +18,13 @@ test('TC1: Screenshot - Compare', async ({ page }) => {
   await page.locator('#login-form_username').fill('admin');
   await page.locator('#login-form_password').fill('');
   await page.locator('button:has-text("Đăng nhập")').click();
-  await page.waitForTimeout(500);
+  await page.waitForTimeout(1000);
 
   // Kiểm tra theo ảnh chụp
   // So sánh ảnh chụp với ảnh đã lưu trước đó
   const screenshot = await page.screenshot({ clip: { x: 0, y: 0, height: 1080, width: 1920 } });
+
+  await page.waitForTimeout(1000);
 
   // Kiểm tra ảnh chụp có khớp với ảnh đã lưu trước đó không?
   expect(screenshot).toMatchSnapshot({
