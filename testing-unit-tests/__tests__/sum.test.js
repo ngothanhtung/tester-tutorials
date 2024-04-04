e3e65450445895889b8950c11af4d3f699adb78d
Ngo Thanh Tung <tungnt@softech.vn>
2024-04-04 11:21:06 +0700

    Update

--- testing-unit-tests/__tests__/sum.test.js
+++ testing-unit-tests/__tests__/sum.test.js
@@ -5,4 +5,8 @@ describe('math', () => {
   test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
+
+  test('adds 5 + 9 to equal 14', () => {
+    expect(sum(5, 9)).toBe(14);
+  });
 });
