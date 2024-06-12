a84e143ecbd767c5a33ea5d1ff2c1e6119e83e8b
Ngo Thanh Tung <tungnt@softech.vn>
2024-06-12 18:05:16 +0700

    Update

---
+++ 02.Examples/Playwright-Batch35/tests/login.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://aptech-tester.web.app/login');
+});
+
+test.describe('Login', () => {
+  test('should login successfully', async ({ page }) => {
+    // await page.getByPlaceholder('Nhập tên đăng nhập').fill('admin');
+    await page.locator('#login-form_username').fill('admin');
+    await page.locator('#login-form_password').fill('Tester@123');
+    await page.locator('#login-form > div:nth-child(4) > div > div > div > button').click();
+
+    await expect(page).toHaveURL('https://aptech-tester.web.app/home');
+  });
+});
