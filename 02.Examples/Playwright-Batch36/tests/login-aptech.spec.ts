f49ba746897c158abf84c97e322825d5a1789c88
Ngo Thanh Tung <tungnt@softech.vn>
2024-10-15 19:36:45 +0700

    Delete files

--- 02.Examples/Playwright-Batch36/tests/login-aptech.spec.ts
+++ 02.Examples/Playwright-Batch36/tests/login-aptech.spec.ts
@@ -1,16 +1,16 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('https://os-admin.aptech.io/login');
+  await page.goto('https://aptech-tester.web.app/lifecycle/login/app');
 });
 
 test('TC1 - Login thành công ', async ({ page }) => {
   // Viết code ...
-  await page.locator('#root > div.account-pages.my-5.pt-sm-5 > div > div > div > div.overflow-hidden.card > div.pt-0.card-body > div.p-2 > form > div:nth-child(1) > input').fill('tungnt@softech.vn');
-  await page.locator('#root > div.account-pages.my-5.pt-sm-5 > div > div > div > div.overflow-hidden.card > div.pt-0.card-body > div.p-2 > form > div:nth-child(2) > input').fill('123456789');
-  await page.locator('#root > div.account-pages.my-5.pt-sm-5 > div > div > div > div.overflow-hidden.card > div.pt-0.card-body > div.p-2 > form > div.mt-3.d-grid > button').click();
+  await page.locator('#login-form_username').fill('tungnt@softech.vn');
+  await page.locator('#login-form_password').fill('123456789');
+  await page.locator('#login-form > div:nth-child(3) > div > div > div > div > button').click();
 
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(3000);
 
-  await expect(page).toHaveURL('https://os-admin.aptech.io/dashboard');
+  await expect(page).toHaveURL('https://aptech-tester.web.app/home');
 });
