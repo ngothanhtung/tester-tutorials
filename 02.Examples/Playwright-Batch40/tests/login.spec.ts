8a269e24998c0b2cbc6723726be765186f1af111
Ngo Thanh Tung <tungnt@softech.vn>
2025-08-13 20:31:48 +0700

    feat: Initialize Playwright Batch 40 example with tests for Todo app, login, and online shop categories
    - Added package.json and playwright.config.ts for Playwright setup.
    - Implemented tests for a demo Todo application covering various functionalities such as adding, editing, and deleting todos.
    - Created example tests for basic Playwright functionalities including title checks and link navigation.
    - Developed generic tests demonstrating various Jest matchers.
    - Added locator tests to validate UI elements and their states.
    - Implemented login tests to verify successful and unsuccessful login scenarios.
    - Created tests for online shop category management including create, update, and delete operations.

---
+++ 02.Examples/Playwright-Batch40/tests/login.spec.ts
@@ -0,0 +1,71 @@
+import { test, expect } from '@playwright/test';
+
+test('Login thành công', async ({ page }) => {
+  await page.goto('https://aptech-tester.web.app/login');
+
+  await page.locator('#login-form_username').fill('admin');
+  await page.locator('#login-form_password').fill('Tester@123');
+
+  await page.locator('#login-form > div:nth-child(4) > div > div > div > div > button').click();
+
+  // Wait for 1000ms
+  await page.waitForTimeout(500);
+  await expect(page).toHaveURL('https://aptech-tester.web.app/home');
+});
+
+// Testcase 2: Đăng nhập không thành công với tài khoản sai thông tin
+test('TC2 - Login: Không thành công', async ({ page }) => {
+  await page.goto('https://aptech-tester.web.app/login');
+
+  // Điền tên đăng nhập
+  await page.locator('#login-form_username').fill('wrong-username');
+
+  // Điền mật khẩu
+  await page.locator('#login-form_password').fill('Tester@123');
+
+  // Click vào nút Đăng nhập
+  await page.locator('button:has-text("Đăng nhập")').click();
+
+  // Wait for 1000ms
+  await page.waitForTimeout(500);
+  // Kiểm tra URL sau khi đăng nhập không thành công
+  await expect(page).toHaveURL('https://aptech-tester.web.app/login');
+});
+
+// Testcase 3: Đăng nhập không thành công với tài khoản sai thông tin
+test('TC3 - Login: Không thành công', async ({ page }) => {
+  await page.goto('https://aptech-tester.web.app/login');
+
+  // Điền tên đăng nhập
+  await page.locator('#login-form_username').fill('admin');
+
+  // Điền mật khẩu
+  await page.locator('#login-form_password').fill('Tester@129');
+
+  // Click vào nút Đăng nhập
+  await page.locator('button:has-text("Đăng nhập")').click();
+
+  // Wait for 1000ms
+  await page.waitForTimeout(500);
+  // Kiểm tra URL sau khi đăng nhập không thành công
+  await expect(page).toHaveURL('https://aptech-tester.web.app/login');
+});
+
+// Testcase 4: Đăng nhập không thành công với tài khoản sai thông tin
+test('TC4 - Login: Không thành công', async ({ page }) => {
+  await page.goto('https://aptech-tester.web.app/login');
+
+  // Điền tên đăng nhập
+  await page.locator('#login-form_username').fill('admix');
+
+  // Điền mật khẩu
+  await page.locator('#login-form_password').fill('Tester@129');
+
+  // Click vào nút Đăng nhập
+  await page.locator('button:has-text("Đăng nhập")').click();
+
+  // Wait for 1000ms
+  await page.waitForTimeout(500);
+  // Kiểm tra URL sau khi đăng nhập không thành công
+  await expect(page).toHaveURL('https://aptech-tester.web.app/login');
+});
